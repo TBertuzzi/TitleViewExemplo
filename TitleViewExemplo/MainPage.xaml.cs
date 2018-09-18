6c9539cf1649174e9e2bc63c0697ef6262200b53
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-09-18 08:20:32 -0300

    :tada: Primeira versão do artigo

---
+++ TitleViewExemplo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TitleViewExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+            Application.Current.MainPage.DisplayAlert("Bacon?", "Bacon é vida!", "ISSO AE");
+        }
+    }
+}
